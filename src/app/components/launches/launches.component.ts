6b691c991d5daaf9ceab3b8d79a3593edb276074
unknown <lienapuey@gmail.com>
2023-05-31 17:43:28 +0200

    first commit

---
+++ src/app/components/launches/launches.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-launches',
+  templateUrl: './launches.component.html',
+  styleUrls: ['./launches.component.scss']
+})
+export class LaunchesComponent {
+
+}
